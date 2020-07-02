6173ad2346ba79736520b51a8cb5f360e326852a
afdolsing <singgih.student@gmail.com>
2020-07-02 19:44:46 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do App</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="main-section">
+        <div class="add-section">
+            <form action="">
+                <input type="text" name="title" placeholder="What is your focus today?">
+                <button type="submit">Add &nbsp; <span>&#43;</span></button>
+            </form>
+        </div>
+        <div class="show-todo-section">
+            <div class="todo-item">
+                <input type="checkbox">
+                <h2>Learn PHP</h2>
+                <br>
+                <small>created :3 6 2020</small>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
